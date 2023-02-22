2b778b13f4666c314689702a9ebfe6dd45927c9a
Michiel A. A. Hoogkamer <michiel.hoogkamer@staffbase.com>
2023-02-22 15:44:17 +0100

    Added methods for returning and new player score posting

--- src/httprequest.class.js
+++ src/httprequest.class.js
@@ -1,6 +1,32 @@
 export default class HttpRequest {
     constructor() {
     }
-    postScoreReturningPlayer() {}
-    postScoreNewPlayer() {}
+    async postScoreReturningPlayer(name, points) {
+        try {
+            const response = await fetch('localhost:8080/returningplayer/', {
+                method: 'post',
+                body: {
+                    "name": name,
+                    "points": points
+                }
+            });
+            console.log('Score has been posted!', response);
+        } catch (err) {
+            console.error(`Error: ${err}`);
+        }
+    }
+    async postScoreNewPlayer(name, points) {
+        try {
+            const response = await fetch('localhost:8080/newplayer/', {
+                method: 'post',
+                body: {
+                    "name": name,
+                    "points": points
+                }
+            });
+            console.log('Score has been posted!', response);
+        } catch (err) {
+            console.error(`Error: ${err}`);
+        }
+    }
 }
