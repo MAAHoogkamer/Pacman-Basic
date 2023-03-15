d5a8a981c5227c36bafd51542c9059de20a96aa1
Michiel A. A. Hoogkamer <michiel.hoogkamer@staffbase.com>
2023-03-15 16:25:55 +0100

    Added a spawn point for enemies

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,8 +1,6 @@
 
 I
 src/touchcontrol.class.js,c/a/cadd73a2042d43c5ee422a60e818cfb55dd0d47c
-;
-GridDef.txt,1/9/190d8d5c46592e6bf952f81b02f4407f30e7167e
 <
 package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
 9
--- src/grid.class.js
+++ src/grid.class.js
@@ -1,45 +1,68 @@
 let gameStatus = {
-    locOf5: null,
-    locOf4Left: null,
-    locOf4Right: null
+    locOf5: {}, // Spawnpoint Communicorn
+    locOf4Left: {}, // Left tunnel entrance
+    locOf4Right: {}, // Right tunnel entrance
+    locOf3: {}, // Spawnpoint enemies
 };
 
 function generateGrid() {
     let grid = [];
-    const numRows = 31;
-    const numCols = 52;
 
-    for (let i = 0; i < numRows; i++) {
+    // Initialize grid with "0"s
+    for (let i = 0; i < 31; i++) {
         let row = [];
-        for (let j = 0; j < numCols; j++) {
-            if (Math.random() < 0.5) {
-                row.push("1");
-            } else {
-                row.push("0");
-            }
+        for (let j = 0; j < 52; j++) {
+            row.push("0");
         }
         grid.push(row);
     }
 
-    // Place a "5" on the grid
-    let row5 = Math.floor(Math.random() * (numRows - 2)) + 1;
-    let col5 = Math.floor(Math.random() * 10) + 1;
-    grid[row5][col5] = "5";
-    gameStatus.locOf5 = {row: row5, col: col5};
+    // Generate pathways
+    for (let i = 1; i < 30; i++) {
+        for (let j = 1; j < 51; j++) {
+            if (Math.random() < 0.5) {
+                grid[i][j] = "1";
+            }
+        }
+    }
 
-    // Place a "4" on the left side of the grid
-    let row4Left = Math.floor(Math.random() * numRows);
-    grid[row4Left][0] = "4";
-    gameStatus.locOf4Left = {row: row4Left, col: 0};
+    // Place left 4
+    let left4Placed = false;
+    while (!left4Placed) {
+        let i = Math.floor(Math.random() * 31);
+        if (grid[i][1] === "1") {
+            grid[i][0] = "4";
+            gameStatus.locOf4Left = { x: 0, y: i };
+            left4Placed = true;
+        }
+    }
 
-    // Place a "4" on the right side of the grid
-    let row4Right = Math.floor(Math.random() * numRows);
-    grid[row4Right][numCols - 1] = "4";
-    gameStatus.locOf4Right = {row: row4Right, col: numCols - 1};
+    // Place right 5
+    let right4Placed = false;
+    while (!right4Placed) {
+        let i = Math.floor(Math.random() * 31);
+        if (grid[i][50] === "1") {
+            grid[i][51] = "5";
+            gameStatus.locOf4Right = { x: 51, y: i };
+            right4Placed = true;
+        }
+    }
+
+    // Place middle 3
+    let middle3Placed = false;
+    while (!middle3Placed) {
+        let i = Math.floor(Math.random() * 29) + 1;
+        let j = Math.floor(Math.random() * 49) + 1;
+        if (grid[i][j] === "1" && grid[i - 1][j] === "1" && grid[i + 1][j] === "1" && grid[i][j - 1] === "1" && grid[i][j + 1] === "1") {
+            grid[i][j] = "3";
+            gameStatus.locOf3 = { x: j, y: i };
+            middle3Placed = true;
+        }
+    }
 
     // Make sure every "1" in the grid is part of a pathway
-    for (let i = 1; i < numRows - 1; i++) {
-        for (let j = 1; j < numCols - 1; j++) {
+    for (let i = 1; i < 30; i++) {
+        for (let j = 1; j < 51; j++) {
             if (grid[i][j] === "0") {
                 let count = 0;
                 if (grid[i - 1][j] === "1") count++;
@@ -49,17 +72,6 @@ function generateGrid() {
                 if (count >= 3) grid[i][j] = "1";
             }
         }
-    }
-
-    // Connect the two "4"s with a tunnel made of "1"s
-    let rowLeft = gameStatus.locOf4Left.row;
-    let rowRight = gameStatus.locOf4Right.row;
-    let colLeft = gameStatus.locOf4Left.col;
-    let colRight = gameStatus.locOf4Right.col;
-
-    while (colLeft <= colRight) {
-        grid[rowLeft][colLeft] = "1";
-        colLeft++;
     }
 
     return grid;
