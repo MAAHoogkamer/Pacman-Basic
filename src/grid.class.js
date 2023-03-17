dcc6a26d0f22c83d057b41c6562f77e82cdddbec
Michiel A. A. Hoogkamer <michiel.hoogkamer@staffbase.com>
2023-03-17 10:01:45 +0100

    Added img for tunnel and wall to be drawn on map. Changed background and code to reflect this.

--- src/grid.class.js
+++ src/grid.class.js
@@ -7,62 +7,48 @@ let gameStatus = {
 
 function generateGrid() {
     let grid = [];
+    const numRows = 31;
+    const numCols = 52;
 
-    // Initialize grid with "0"s
-    for (let i = 0; i < 31; i++) {
+    for (let i = 0; i < numRows; i++) {
         let row = [];
-        for (let j = 0; j < 52; j++) {
-            row.push("0");
+        for (let j = 0; j < numCols; j++) {
+            if (Math.random() < 0.5) {
+                row.push("1");
+            } else {
+                row.push("0");
+            }
         }
         grid.push(row);
     }
 
-    // Generate pathways
-    for (let i = 1; i < 30; i++) {
-        for (let j = 1; j < 51; j++) {
-            if (Math.random() < 0.5) {
-                grid[i][j] = "1";
-            }
-        }
-    }
+    // Place a "5" on the grid
+    let row5 = Math.floor(Math.random() * (numRows - 2)) + 1;
+    let col5 = Math.floor(Math.random() * 10) + 1;
+    grid[row5][col5] = "5";
+    gameStatus.locOf5 = {row: row5, col: col5};
 
-    // Place left 4
-    let left4Placed = false;
-    while (!left4Placed) {
-        let i = Math.floor(Math.random() * 31);
-        if (grid[i][1] === "1") {
-            grid[i][0] = "4";
-            gameStatus.locOf4Left = { x: 0, y: i };
-            left4Placed = true;
-        }
-    }
+    // Place a "4" on the left side of the grid
+    let row4Left = Math.floor(Math.random() * numRows);
+    grid[row4Left][0] = "4";
+    gameStatus.locOf4Left = {row: row4Left, col: 0};
 
-    // Place right 5
-    let right4Placed = false;
-    while (!right4Placed) {
-        let i = Math.floor(Math.random() * 31);
-        if (grid[i][50] === "1") {
-            grid[i][51] = "5";
-            gameStatus.locOf4Right = { x: 51, y: i };
-            right4Placed = true;
-        }
-    }
+    // Place a "4" on the right side of the grid
+    let row4Right = Math.floor(Math.random() * numRows);
+    grid[row4Right][numCols - 1] = "4";
+    gameStatus.locOf4Right = {row: row4Right, col: numCols - 1};
 
-    // Place middle 3
-    let middle3Placed = false;
-    while (!middle3Placed) {
-        let i = Math.floor(Math.random() * 29) + 1;
-        let j = Math.floor(Math.random() * 49) + 1;
-        if (grid[i][j] === "1" && grid[i - 1][j] === "1" && grid[i + 1][j] === "1" && grid[i][j - 1] === "1" && grid[i][j + 1] === "1") {
-            grid[i][j] = "3";
-            gameStatus.locOf3 = { x: j, y: i };
-            middle3Placed = true;
-        }
+    let i = Math.floor(Math.random() * 29) + 1;
+    let j = Math.floor(Math.random() * 49) + 1;
+    if (grid[i][j] === "1" && grid[i - 1][j] === "1" && grid[i + 1][j] === "1" && grid[i][j - 1] === "1" && grid[i][j + 1] === "1") {
+        grid[i][j] = "3";
+        gameStatus.locOf3 = {x: j, y: i};
+        middle3Placed = true;
     }
 
     // Make sure every "1" in the grid is part of a pathway
-    for (let i = 1; i < 30; i++) {
-        for (let j = 1; j < 51; j++) {
+    for (let i = 1; i < numRows - 1; i++) {
+        for (let j = 1; j < numCols - 1; j++) {
             if (grid[i][j] === "0") {
                 let count = 0;
                 if (grid[i - 1][j] === "1") count++;
@@ -73,6 +59,18 @@ function generateGrid() {
             }
         }
     }
+    /*
+    // Connect the two "4"s with a tunnel made of "1"s
+    let rowLeft = gameStatus.locOf4Left.row;
+    let rowRight = gameStatus.locOf4Right.row;
+    let colLeft = gameStatus.locOf4Left.col;
+    let colRight = gameStatus.locOf4Right.col;
+
+    while (colLeft <= colRight) {
+        grid[rowLeft][colLeft] = "1";
+        colLeft++;
+    }
+     */
 
     return grid;
 }
--- src/map.class.js
+++ src/map.class.js
@@ -9,7 +9,10 @@ export default class Map {
         this.CANVAS = document.getElementById("canvas");
         this.CTX = this.CANVAS.getContext("2d");
         // Load the images:
-        this.IMAGES = ['./img/UnicornRight.png', './img/UnicornLeft.png', './img/UnicornDown.png', './img/UnicornUp.png', './img/UnicornRight1.png', './img/UnicornLeft1.png', './img/UnicornDown1.png', './img/UnicornUp1.png', './img/Ghost1.png', './img/Ghost2.png', './img/NewBluePill.png', './img/YellowPill.png'].map((src) => {
+        this.IMAGES = ['./img/UnicornRight.png', './img/UnicornLeft.png', './img/UnicornDown.png',
+            './img/UnicornUp.png', './img/UnicornRight1.png', './img/UnicornLeft1.png', './img/UnicornDown1.png',
+            './img/UnicornUp1.png', './img/Ghost1.png', './img/Ghost2.png', './img/NewBluePill.png',
+            './img/YellowPill.png', './img/Wall.png', './img/Tunnel.png'].map((src) => {
             const IMG = new Image();
             IMG.src = src;
             return IMG;
@@ -28,17 +31,23 @@ export default class Map {
         this.CTX.clearRect(0, 0, 720, 540);
         this.gameStatus.rows.forEach((row, index) => {
             for (let i = 0; i < row.length; i++) {
-                if (row[i] === '1') {
+                if (row[i] === '1') { // YellowPill
                     this.CTX.drawImage(this.IMAGES[11],i * this.PILL_WIDTH + this.OFFSET_X,(index * this.PILL_WIDTH) + this.OFFSET_Y,6,6);
-                }else if (row[i] === '5') {
+                }else if (row[i] === '5') { // Communicorn
                     this.CTX.drawImage(this.IMAGES[this.whichUniPic],i * this.PILL_WIDTH + this.OFFSET_X - 4.5,(index * this.PILL_WIDTH) + this.OFFSET_Y - 4.5,17,17);
-                }else if (row[i] === '2') {
+                }else if (row[i] === '2') { // Blue Pill
                     this.CTX.drawImage(this.IMAGES[10],i * this.PILL_WIDTH + this.OFFSET_X - 4.5,(index * this.PILL_WIDTH) + this.OFFSET_Y - 4.5,14,14);
-                }else if (row[i] === '6') {
+                }else if (row[i] === '6') { // Bad enemy
                     this.CTX.drawImage(this.IMAGES[8],i * this.PILL_WIDTH + this.OFFSET_X - 4.5,(index * this.PILL_WIDTH) - 4.5+ this.OFFSET_Y,17,17);
-                }else if (row[i] === '7') {
+                }else if (row[i] === '7') { // Weak enemy
                     this.CTX.drawImage(this.IMAGES[9],i * this.PILL_WIDTH + this.OFFSET_X - 4.5,(index * this.PILL_WIDTH) - 4.5+ this.OFFSET_Y,15,15);
+                }else if (row[i] === '0') { // Wall
+                this.CTX.drawImage(this.IMAGES[12],i * this.PILL_WIDTH + this.OFFSET_X - 2,(index * this.PILL_WIDTH) - 2+ this.OFFSET_Y,9,9);
+                }else if (row[i] === '4') { // Tunnel
+                    this.CTX.drawImage(this.IMAGES[13],i * this.PILL_WIDTH + this.OFFSET_X - 4.5,(index * this.PILL_WIDTH) - 4.5+ this.OFFSET_Y,15,15);
                 }
+
+
             }
         })
     }
